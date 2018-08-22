0f772ec92678deb071d6b0630a29e8d8d099191c
Premal Singh <premals98@gmail.com>
2018-08-22 22:17:39 +0530

    Authentication Done.

--- app/src/main/java/com/example/premal2/rvceconnect/MainActivity.java
+++ app/src/main/java/com/example/premal2/rvceconnect/MainActivity.java
@@ -1,16 +1,60 @@
 package com.example.premal2.rvceconnect;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity
 {
 
+    String cred1;
+    String cred2;
+    private FirebaseAuth mAuth;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int x=5;
+        Log.d("e","works fine");
+        mAuth=FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser()!=null)
+        {
+
+            finish();
+            startActivity(new Intent(getApplicationContext(),StudentFrontEnd.class));
+        }
+        final EditText emailid = (EditText) findViewById(R.id.emailid);
+        final EditText password = (EditText) findViewById(R.id.password);
+        Button signin = (Button) findViewById(R.id.signin);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cred1=emailid.getText().toString();
+                cred2=password.getText().toString();
+                mAuth.signInWithEmailAndPassword(cred1,cred2).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Log.d("e","success");
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),StudentFrontEnd.class));
+                        }
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/premal2/rvceconnect/StudentFrontEnd.java
+++ app/src/main/java/com/example/premal2/rvceconnect/StudentFrontEnd.java
@@ -1,13 +1,22 @@
 package com.example.premal2.rvceconnect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class StudentFrontEnd extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    int h;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_front_end);
+        mAuth=FirebaseAuth.getInstance();
+        mAuth.signOut();
+        finish();
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
