78de6c45ffe1bc122184fc1827306ee1554d6453
shubhamphal <shubhamphal13@gmail.com>
2018-10-01 00:52:57 +0530

    Counsellor UI updates

--- app/src/main/java/com/example/premal2/rvceconnect/TeacherCounsel.java
+++ app/src/main/java/com/example/premal2/rvceconnect/TeacherCounsel.java
@@ -1,9 +1,17 @@
 package com.example.premal2.rvceconnect;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TeacherCounsel extends Fragment
 {
@@ -22,9 +30,51 @@ public class TeacherCounsel extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.teacher_counsel, container, false);
+
+        ArrayList<String> studentnames=new ArrayList<>();
+
+        studentnames.add("SHUBHAM");
+        studentnames.add("PREMAL");
+
+        View view =inflater.inflate(R.layout.counsel_list_layout, container, false);
 
 
+        ListView lv=view.findViewById(R.id.counsellingstudentlist);
+
+
+        ArrayAdapter<String> attadapter=new ArrayAdapter<String>(getContext(),R.layout.card_for_teacher_counsel,R.id.studentnamedisplay,studentnames);
+
+
+        lv.setAdapter(attadapter);
+
+
+
+
+
+
+     lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+         @Override
+         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+             Log.i("CLICKED","CLICK");
+             startActivity(new Intent(getActivity().getApplicationContext(),TeacherStudentGaurdianContact.class));
+
+             return;
+         }
+     });
+
+
+
+
+
+
+
+
+
+
+
+
+        return view;
 
     }
 
---
+++ app/src/main/java/com/example/premal2/rvceconnect/TeacherStudentGaurdianContact.java
@@ -0,0 +1,14 @@
+package com.example.premal2.rvceconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherStudentGaurdianContact extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_student_gaurdian_contact);
+    }
+}
